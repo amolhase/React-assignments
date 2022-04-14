c92fa82b6e28186a591c79797fa38adace4e0351
amolhase <amolhase110@gmail.com>
2022-04-14 10:16:29 -0700

    login system created

---
+++ Login System/login.js
@@ -0,0 +1,20 @@
+
+
+function verifyUser(){
+    let userName=document.getElementById("user").value;
+    let pass=document.getElementById("pass").value;  
+   if(userName === "Amol" && pass === "123")
+   {
+    location.href = 'home.html';
+   }
+   else if(userName === "" || pass === "") {
+       alert("Must enter username and password");
+   } else {
+       alert("UserName and Password Not Match");
+   }
+}
+
+function resetUser() {
+    document.getElementById("user").value = "";
+    document.getElementById("pass").value = "";
+}
